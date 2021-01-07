63fd1779303b2a3a6eb386aeb6ad96df2ee51580
darrylt <4dtait@gmail.com>
2021-01-06 18:21:59 -0700

    adds client side routing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,11 @@
+import styles from './Footer.module.css';
+
 function Footer(props) {
     return (
 
-        <footer>Footer</footer>
+        <footer className={styles.Footer}>
+            <p>Copyright &copy; {new Date().getFullYear()} Project 3 Dareal</p>
+        </footer>
     )
     
 }
